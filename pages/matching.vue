85b715871ae0b06f29f578d3bf71beec0c662417
Katsuya Suzuki <angelattack9759@gmail.com>
2021-07-22 15:36:30 +0900

    [Add] マッチング後のトップへ戻るボタン

--- pages/matching.vue
+++ pages/matching.vue
@@ -3,6 +3,7 @@
     <h1>マッチングしました</h1>
     <h2>name: サイトウさん</h2>
     <NuxtLink to="/calling" class="button">通話する</NuxtLink>
+    <NuxtLink to="/" class="button">トップへ戻る</NuxtLink>
   </div>
 </template>
 
