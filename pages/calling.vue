5638244144aba3f6e101839f7f6d0ab0c3cc53f3
Katsuya Suzuki <angelattack9759@gmail.com>
2021-07-22 15:33:57 +0900

    [Add] 通話中画面

---
+++ pages/calling.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>通話中...</h1>
+    <NuxtLink to="/" class="button">通話終了</NuxtLink>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
--- pages/matching.vue
+++ pages/matching.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>マッチングしました</h1>
     <h2>name: サイトウさん</h2>
-    <NuxtLink to="/videochat" class="button">通話する</NuxtLink>
+    <NuxtLink to="/calling" class="button">通話する</NuxtLink>
   </div>
 </template>
 
