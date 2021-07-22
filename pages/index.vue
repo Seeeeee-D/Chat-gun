511493a647a867963c231ef6b5ae56cb72a7626f
Katsuya Suzuki <angelattack9759@gmail.com>
2021-07-22 15:10:41 +0900

    [Add] 通話相手を見つけるボタン
    Co-authored-by: niwabarubossa <niwabarubossa@users.noreply.github.com>
    Co-authored-by: nanaka0012 <nanaka0012@users.noreply.github.com>
    Co-authored-by: Mingzhe Yang <mei28@users.noreply.github.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>Hello world!</h1>
+  <div>
+    <h1>Chat-gun</h1>
+    <button type="button" @click="buttonClicked">通話相手を見つける</button>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    buttonClicked:function () {
+      console.log("Calling...")
+    }
+  }
+}
+</script>
+
+<style></style>
