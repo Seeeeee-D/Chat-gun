02ec724bda8467f114909b226dde92a26d6706cc
Katsuya Suzuki <angelattack9759@gmail.com>
2021-07-22 15:51:28 +0900

    [Add] ハードコードしたユーザとランダムにマッチングさせる機能

--- pages/matching.vue
+++ pages/matching.vue
@@ -1,12 +1,34 @@
 <template>
   <div>
     <h1>マッチングしました</h1>
-    <h2>name: サイトウさん</h2>
+    <h2>name: {{ matchedUser }}</h2>
     <NuxtLink to="/calling" class="button">通話する</NuxtLink>
     <NuxtLink to="/" class="button">トップへ戻る</NuxtLink>
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      users: [
+        "サイトウさん",
+        "Aさん",
+        "Bさん"
+      ],
+      matchedUser: "",
+    }
+  },
+  created() {
+    this.matchedUser = this.selectRandomUser();
+  },
+  methods: {
+    selectRandomUser: function(){
+      let index = Math.floor(Math.random() * this.users.length)
+      return this.users[index]
+    }
+  },
+}
+</script>
 
 <style></style>
